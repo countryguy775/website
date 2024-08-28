140e3b32ff33db7846e93667c704e14f4028d77d
countryguy775 <139970256+countryguy775@users.noreply.github.com>
2024-08-28 10:54:53 -0400

    func

--- backend/VisitFunc/__init__.py
+++ backend/VisitFunc/__init__.py
@@ -1,3 +1,4 @@
+import logging
 
 import azure.functions as func
 
@@ -5,6 +6,6 @@ from .visits import count_updater
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    #logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed a request.')
 
     return count_updater(req)
